028ede1f6dc3ef6c541f064a58fb1258016ff323
Mathias Helsen <helsen.mathias@gmail.com>
2015-02-02 15:00:21 +0100

    rx not working

--- FPGA_ADC.v
+++ FPGA_ADC.v
@@ -1,16 +1,21 @@
-module FPGA_ADC( BUTTON[2:0], HEX0_D[6:0], HEX1_D[6:0], CLOCK_50, GPIO0_D[8:0], UART_TXD );
+module FPGA_ADC( BUTTON[2:0], HEX0_D[6:0], HEX1_D[6:0], HEX2_D[6:0], HEX3_D[6:0], CLOCK_50, GPIO0_D[8:0], UART_TXD, UART_RXD );
 
 input [2:0]BUTTON;
 input CLOCK_50;
 inout [8:0]GPIO0_D;
+input UART_RXD;
 
 output [6:0]HEX0_D;
 output [6:0]HEX1_D;
+output [6:0]HEX2_D;
+output [6:0]HEX3_D;
 output UART_TXD;
 
 wire ADC_Clk;
 wire UART_Clk;
+wire UART_Rx_Clk;
 wire [7:0]binaryOut;
+wire [7:0] rxout;
 wire compIn;
 
 reg [7:0] UART_Data;
@@ -22,26 +27,16 @@ assign compIn = GPIO0_D[8];
 
 assign GPIO0_D[7:0] = binaryOut;
 
-ADC_PLL ADC_pll (, CLOCK_50, ADC_Clk, UART_Clk,);
+ADC_PLL ADC_pll (, CLOCK_50, ADC_Clk, UART_Clk, UART_Rx_Clk, );
 
 SAR_ADC adc(ADC_Clk, compIn, BUTTON[0], , binaryOut);
 
 segdriver hex0(binaryOut[7:4], HEX0_D[6:0]);
 segdriver hex1(binaryOut[3:0], HEX1_D[6:0]);
+segdriver hex2(rxout[3:0], HEX2_D[6:0]);
+segdriver hex3(rxout[7:4], HEX3_D[6:0]);
 
 UART_TX UART_module( UART_Clk, UART_Data, UART_Start, UART_TXD, UART_Done);
-
-always @(posedge CLOCK_50) begin
-    if( UART_Done & ~BUTTON[0] ) begin
-	UART_Start <= 1'b1;
-	case(UART_Data)
-	    8'h00: UART_Data <= 8'h41;
-	    8'h41: UART_Data <= 8'h42;
-	    8'h42: UART_Data <= 8'h0A;
-	    8'h0A: UART_Data <= 8'h0D;
-	    8'h0D: UART_Data <= 8'h00;
-	endcase
-    end
-end
+UART_RX UART_Rx_Module( UART_Rx_Clk, 8'h10, UART_RXD, , rxout);
 
 endmodule
--- UART_RX.v
+++ UART_RX.v
@@ -1,6 +1,7 @@
 module UART_RX( Sample_Clk, Divider, serin, flag, Data_Out);
 
 `define STATE_LISTENING 0
+`define STATE_VERIFY 1
 `define STATE_DELAY 1
 `define STATE_RECEIVING 2
 
@@ -16,24 +17,27 @@ reg [7:0] bitCtr;
 reg [8:0] delayReg;
 reg dataRcvd;
 
-wire [8:0] delayVal;
+reg [8:0] delayVal;
+
+wire frameStart;
 
 assign Data_Out = dataReg;
-assign delayVal = (state == `STATE_DELAY) ? Divider + (Divider >> 1) : (Divider);
 assign flag = dataRcvd;
+assign frameStart = ((state == `STATE_LISTENING) && (~serin));
 
-always @(posedge Sample_Clk, negedge serin) begin
-    if((state == `STATE_LISTENING) && (~serin)) begin
+always @(posedge Sample_Clk ) begin
+    if(state == `STATE_LISTENING && !serin ) begin
 	// We were listening, and now the level has gone low
 	// Before sampling the first bit, we have to wait 3/2 T
 	// and sample at half the first bit to avoid bounce and other stuff
-	state <= `STATE_DELAY;
+	state <= `STATE_VERIFY;
 	delayReg <= 9'h00;
+	delayVal <= Divider + (Divider >> 1);
+	dataReg <= 8'h0;
 	bitCtr <= 8'h0;
 	dataRcvd <= 1'b0;
-    end else if( state != `STATE_LISTENING) begin
-	// If we were instructed to delay for 3/2 T...
-	if( state == `STATE_DELAY ) begin
+    // If we were instructed to delay for 3/2 T...
+    end else if( state == `STATE_DELAY ) begin
 	    // Increase the delay timer
 	    delayReg <= delayReg + 1'b1;
 	    // and check if we are at or beyond the delay time
@@ -41,28 +45,30 @@ always @(posedge Sample_Clk, negedge serin) begin
 		// If so, we receive the first bit and set the delay timer
 		// back to zero
 		delayReg <= 9'h00;
+		delayVal <= Divider;
 		state <= `STATE_RECEIVING;	
 	    end
 	// We are in the active phase of receiving
-	end else begin
-	    // If the timer has been reset to zero, it is time to sample a bit
-	    if( delayReg == 9'h00 ) begin
+    end else if ( state == `STATE_RECEIVING ) begin
+	// If the timer has been reset to zero, it is time to sample a bit
+	if( delayReg == 9'h00 ) begin
+
+	    bitCtr <= bitCtr + 1'b1;
+	    if(bitCtr <= 8'h8)
 		dataReg <= {serin, dataReg[7:1]};
-		bitCtr <= bitCtr + 1'b1;
-		// Check if we have received all 8 bits
-		if( bitCtr >= 8'h8 ) begin
-		    state <= `STATE_LISTENING; 
-		    dataRcvd <= 1'b1;
-		end else begin
-		    // If not, increase the delay timer again
-		    delayReg <= delayReg + 1'b1;	
-		end
-	    // We increase the timer and check for overflows 
+	    // Check if we have received all 8 bits
+	    if( bitCtr > 8'h9 ) begin
+		state <= `STATE_LISTENING; 
+		dataRcvd <= 1'b1;
 	    end else begin
-		delayReg <= delayReg + 1'b1;
-		if(delayReg >= delayVal)
-		    delayReg <= 9'h0;
+		// If not, increase the delay timer again
+		delayReg <= delayReg + 1'b1;	
 	    end
+	// We increase the timer and check for overflows 
+	end else begin
+	    delayReg <= delayReg + 1'b1;
+	    if(delayReg >= delayVal)
+		delayReg <= 9'h0;
 	end
     end
 end
